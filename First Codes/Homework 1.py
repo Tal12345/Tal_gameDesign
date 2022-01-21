71ab14f22dbb4880665573d43f1f038fe7ebd19f
Tal12345 <talrogozinski@gmail.com>
2022-01-21 09:01:11 -0600

    update

---
+++ First Codes/Homework 1.py
@@ -0,0 +1,9 @@
+print(" Greenhill : 10, Portal : 5.33")
+print("Total students : 240, Boys : 120")
+print("031")
+print("3.561E+02")
+
+print("Welcome to", end=" ")
+print("Greenhill School")
+
+print("I\nLove\tGreenhill")
