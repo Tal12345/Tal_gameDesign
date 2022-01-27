5c6aee78ca314c808240fabe7b21ba4b16ce2b8b
Tal12345 <talrogozinski@gmail.com>
2022-01-26 22:32:54 -0600

    update

--- First Codes/userintput.py
+++ First Codes/userintput.py
@@ -13,24 +13,52 @@ os.system('cls')
 
 #guess a number
 #myNumber = 9 instead of using a fixed number we will use a random
-myNumber=random.randint(1,10)
-print("################################################################")
-print("#                                                              #")
-print("# Guess a Number Menu  #") 
-print ("Welcome to the NUMBER GAME :))")     
-print(' choose a game! ')
-print ('')
-print ('choose 1 for 1-10.')
-print ('choose 2 for 1-50')
-print ('choose 3 for 1-100')
+#Lets make menu a funcion key word def
+myNumber=0
+def menu():
+    print("-----------------------------------------------------------------")
+    print("-                           WELCOME                             -")   
+    print("-                      Guess a Number Game                      -")
+    print("-                                                               -")
+    print("-                        Level 1 = 1-10                         -")
+    print("-                        Level 2 = 1-50                         -")
+    print("-                        Level 3 = 1-100                        -")
+    print("-                       Select Your Level                       -")
+    print("-----------------------------------------------------------------")
+#Checking for correct user input
+menu()
+check=True
+while check:
+    try:
+        level=int(input("Level: "))
+        check= False
+    except ValueError:
+        print("Sorry thats not a level, please enter 1 to 3 only!")
 
+    
+if level == 1:
+    myNumber= random.randint(1,10)
+elif level == 2:
+    myNumber= random.randint(1,50)
+elif level == 3:
+    myNumber= random.randint(1,100)
+print(level)
 
 GameOn=True
 while(GameOn):
-    userGuess=int(input("Guess a number from 1-10 "))
+    userGuess=int(input("Give me a number: "))
     if myNumber ==userGuess:
         print("You guessed it!!!")
         GameOn=False
     else:
-        print("better luck next time:((")
+        print("better luck next time:(( the number was", myNumber)
 print("The number to guess was "+ str(myNumber))
+os.system('cls')
+menu()
+
+
+
+
+
+
+
